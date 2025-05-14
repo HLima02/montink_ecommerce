1246f6b58eac15f7020a58cdebab7bcac1088745
HLima02 <hiago.liima02@hotmail.com>
2025-05-14 13:43:00 -0300

    Adição de links nos ícones do rodapé

--- src/app/componentes/Footer/index.tsx
+++ src/app/componentes/Footer/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
+import Link from 'next/link';
 import { FaInstagram, FaYoutube  } from "react-icons/fa";
 import logo from '@/assets/logo.png'
 
@@ -12,9 +13,9 @@ export default function Footer() {
       <div className='w-full flex justify-between border-t-1 border-white border-solid py-2'>
         <h2 className=' text-2xl font-bold'>MontinkCommerce</h2>
         <div className='flex gap-2'>
-          <span className='bg-white flex w-7 h-7 rounded-full justify-center'><Image src={logo} alt='Logo Montink' width={50} height={50} /></span>
-          <FaYoutube size={30}/>
-          <FaInstagram size={30}/>
+          <Link href='https://montink.com/' className='bg-white flex w-7 h-7 rounded-full justify-center'><Image src={logo} alt='Logo Montink' width={50} height={50} /></Link>
+          <Link href="https://www.youtube.com/@montinkoficial"><FaYoutube size={30}/></Link>
+          <Link href="https://www.instagram.com/soumontink/"><FaInstagram size={30}/></Link>
         </div>
       </div>
     </header>
